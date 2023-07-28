f14e0ad37cb49b6695624b2455a7ad2ac8f92219
yp <yinpan0227@hotmail.com>
2023-07-28 17:09:43 +0800

    提交work

---
+++ Pool.hpp
@@ -0,0 +1,102 @@
+#pragma once
+#include <memory>
+#include <queue>
+#include <mutex>
+#include <future>
+
+class function_wrapper
+{
+	struct impl_base {
+		virtual void call() = 0;
+		virtual ~impl_base() {}
+	};
+	std::unique_ptr<impl_base> impl;
+	template<typename F>
+	struct impl_type : impl_base
+	{
+		F f;
+		impl_type(F&& f_) : f(std::move(f_)) {}
+		void call() { f(); }
+	};
+public:
+	template<typename F>
+	function_wrapper(F&& f) :
+	impl(new impl_type<F>(std::move(f)))
+	{}
+	void operator()() { impl->call(); }
+	function_wrapper() = default;
+	function_wrapper(function_wrapper && other) :
+		impl(std::move(other.impl))
+	{}
+	function_wrapper& operator=(function_wrapper&& other)
+	{
+		impl = std::move(other.impl);
+		return *this;
+	}
+	function_wrapper(const function_wrapper&) = delete;
+	function_wrapper(function_wrapper&) = delete;
+	function_wrapper& operator=(const function_wrapper&) = delete;
+};
+
+template <typename T>
+class thread_safe_queue {
+public:
+	thread_safe_queue() {}
+
+	void enqueue(const T& item) {
+		std::lock_guard<std::mutex> lock(mutex_);
+		queue_.push(item);
+	}
+	bool dequeue(T& item) {
+		std::lock_guard<std::mutex> lock(mutex_);
+		if (queue_.empty()) {
+			return false;
+		}
+		item = queue_.front();
+		queue_.pop();
+		return true;
+	}
+
+	bool empty() const {
+		std::lock_guard<std::mutex> lock(mutex_);
+		return queue_.empty();
+	}
+
+private:
+	std::queue<T> queue_;
+	mutable std::mutex mutex_;
+};
+
+class thread_pool
+{
+	std::atomic_bool done;
+	thread_safe_queue<function_wrapper> work_queue; // 使用function_wrapper，而非使用std:
+	
+	void worker_thread()
+	{
+		while (!done)
+		{
+			function_wrapper task;
+			if (work_queue.dequeue(task))
+			{
+				task();
+			}
+			else
+			{
+				std::this_thread::yield();
+			}
+		}
+	}
+public:
+	template<typename FunctionType>
+	std::future<typename std::result_of<FunctionType()>::type> // 1
+		submit(FunctionType f)
+	{
+		typedef typename std::result_of<FunctionType()>::type
+			result_type; // 2
+		std::packaged_task<result_type()> task(std::move(f)); // 3
+		std::future<result_type> res(task.get_future()); // 4
+		work_queue.push(std::move(task)); // 5
+		return res; // 6
+	}
+};
